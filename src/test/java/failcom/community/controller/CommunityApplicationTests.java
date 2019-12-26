54661cac806718a2808cf1c8d42853cd372aa5ea
failcom <1257080201@qq.com>
2019-12-26 12:46:44 +0800

    init repo

---
+++ src/test/java/failcom/community/controller/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package failcom.community.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
